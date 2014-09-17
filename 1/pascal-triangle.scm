2fb648d9e90bdcdb3b6af882e33b3d8b0836fb6c
Michael Liao <askxuefeng@gmail.com>
2014-09-17 09:33:22 +0800

    add comments

--- 1/pascal-triangle.scm
+++ 1/pascal-triangle.scm
@@ -6,6 +6,12 @@
 ;; 1 4 6 4 1
 
 (define (p-triangle n)
+  ;; the next line can be built by:
+  ;; current line: (1 a b c b a 1)
+  ;; insert 0:     (0 1 a b c b a 1)
+  ;; append 0:     (1 a b c b a 1 0)
+  ;; apply      + -------------------
+  ;;               (1 ? ? ? ? ? ? 1)
   (define (next-triangle lst)
     (map + (cons 0 lst)
            (append lst '(0))))
