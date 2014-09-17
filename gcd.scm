4870c28ceef1d310ed788856531ff270d2412745
Michael Liao <askxuefeng@gmail.com>
2014-09-17 09:18:27 +0800

    add gcd

---
+++ gcd.scm
@@ -0,0 +1,23 @@
+;; greatest common divisors
+
+(define (gcd a b)
+  (if (= b 0)
+      a
+      (gcd b (mod a b))))
+
+;; test
+
+(display "(gcd 1 20) ==> ")
+(display (gcd 1 20))
+(newline)
+
+(display "(gcd 10 15) ==> ")
+(display (gcd 10 15))
+(newline)
+
+(display "(gcd 42 24) ==> ")
+(display (gcd 42 24))
+(newline)
+
+(display "(gcd 23 31) ==> ")
+(display (gcd 23 31))
