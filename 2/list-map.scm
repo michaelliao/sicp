a92c5aebd03fb57ba36119f41e7e8dedc5882167
Michael Liao <askxuefeng@gmail.com>
2014-10-11 16:55:35 +0800

    add map/op

---
+++ 2/list-map.scm
@@ -0,0 +1,19 @@
+(define (map-list func items)
+  (if (null? items)
+      '()
+      (cons (func (car items))
+            (map-list func (cdr items)))))
+
+(define (scale-list items factor)
+  (map-list (lambda (x)
+               (* x factor))
+            items))
+
+;; test
+
+(display (scale-list '(1 2 3 4 5) 2))
+(newline)
+(display (scale-list '(1 3 5 7 9) 9))
+(newline)
+(display (map-list (lambda (x) (* x x x)) '(1 2 3 4 5)))
+(newline)
---
+++ 2/list-op.scm
@@ -0,0 +1,36 @@
+(define (list-ref items n)
+  (if (= n 0)
+      (car items)
+      (list-ref (cdr items) (- n 1))))
+
+(define (length items)
+  (define (length-iter items n)
+    (if (null? items)
+        n
+        (length-iter (cdr items) (+ n 1))))
+  (length-iter items 0))
+
+(define (last items)
+  (let ((first (car items))
+        (rest (cdr items)))
+    (if (null? rest)
+        first
+        (last rest))))
+
+;; test
+
+(display (list-ref (list "A" "B" "C" "D") 0))
+(newline)
+(display (list-ref '("A" "B" "C" "D") 1))
+(newline)
+(display (list-ref '("A" "B" ("C" "D")) 2))
+(newline)
+(display (length '(1 2 3 4 5)))
+(newline)
+(display (length '(1 2 (3 4) 5)))
+(newline)
+(display (last '(1 2 3 4 5)))
+(newline)
+(display (last '(1 2 3 '(4 5))))
+(newline)
+
