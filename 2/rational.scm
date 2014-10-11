2312ede51d5cfb9e8243c7aa38dc02ec420dca80
Michael Liao <askxuefeng@gmail.com>
2014-10-11 11:32:40 +0800

    update rational

--- 2/rational.scm
+++ 2/rational.scm
@@ -13,9 +13,38 @@
   (display (denom x))
   (newline))
 
+(define (add-rat x y)
+  (make-rat (+ (* (number x) (denom y))
+               (* (number y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (sub-rat x y)
+  (make-rat (- (* (number x) (denom y))
+               (* (number y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (mul-rat x y)
+  (make-rat (* (number x) (number y))
+            (* (denom x) (denom y))))
+
+(define (div-rat x y)
+  (make-rat (* (number x) (denom y))
+            (* (denom x) (number y))))
+
+(define (equal-rat? x y)
+  (= (* (number x) (denom y))
+     (* (number y) (denom x))))
+
 ;; test
 
 (print-rat (make-rat 1 2))
 
 (print-rat (make-rat 2 3))
 
+(print-rat (add-rat (make-rat 1 2) (make-rat 1 3)))
+
+(print-rat (sub-rat (make-rat 1 2) (make-rat 1 3)))
+
+(print-rat (mul-rat (make-rat 1 2) (make-rat 2 3)))
+
+(print-rat (div-rat (make-rat 1 2) (make-rat 1 3)))
