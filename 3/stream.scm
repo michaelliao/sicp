8cb92fea65d3f802051fe0bdd7109cbd397dca4b
Michael Liao <askxuefeng@gmail.com>
2015-02-10 16:12:49 +0800

    update stream.scm

--- 3/stream.scm
+++ 3/stream.scm
@@ -1,3 +1,5 @@
+#!/usr/bin/env petite
+
 (define-syntax delay
   (syntax-rules ()
     ((_ exp) (lambda () exp))))
@@ -45,19 +47,28 @@
   (cons-stream (apply proc (map stream-car streams))
                (apply stream-map (cons proc (map stream-cdr streams)))))
 
+;; test
+
 (define (print-stream s)
+  ;; print the first n elements of stream
   (define (do-print s n)
     (if (= n 0)
-        (newline)
+        (begin
+          (display "...")
+          (newline))
         (begin
           (display (stream-car s))
           (display ", ")
           (do-print (stream-cdr s) (- n 1)))))
   (do-print s 20))
 
+;; stream: 1, 1, 1, 1, 1, ...
+
 (define ones (cons-stream 1 ones))
 
 (print-stream ones)
+
+;; stream: 1, 2, 3, 4, 5, ...
 
 (define (add-streams s1 s2)
   (stream-map + s1 s2))
@@ -66,6 +77,8 @@
 
 (print-stream integers)
 
+;; fibonacci: 0, 1, 1, 2, 3, ...
+
 (define (fib a b)
   (cons-stream a (fib b (+ a b))))
 
@@ -73,3 +86,4 @@
 
 (print-stream fib-stream)
 
+(exit)
