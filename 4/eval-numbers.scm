0a853c08a66c99b110150b730d1cb4a82ccbaaa3
Michael Liao <askxuefeng@gmail.com>
2015-02-10 16:17:32 +0800

    add display

--- 4/eval-numbers.scm
+++ 4/eval-numbers.scm
@@ -1,4 +1,6 @@
-;; calculate numbers
+#!/usr/bin/env petite
+
+;; eval S-expression contains only +, -, *
 
 (define (eval-exp exp)
   (cond ((list? exp) (eval-list exp))
@@ -7,7 +9,7 @@
 
 (define (eval-list exp)
   (calc-exp (car exp)
-         (map eval-exp (cdr exp))))
+            (map eval-exp (cdr exp))))
 
 (define (calc-exp op args)
   (cond ((procedure? op) (apply op args))
@@ -16,10 +18,19 @@
 
 ;; test
 
+(display "(* 1 2 3 4 5) = ")
 (display (eval-exp (list * 1 2 3 4 5)))
 (newline)
+
+(display "(+ 1 2 3) = ")
 (display (eval-exp '(+ 1 2 3)))
 (newline)
+
+(display "(* (+ 2 3) (- 9 4)) = ")
 (display (eval-exp '(* (+ 2 3) (- 9 4))))
 (newline)
+
+;; should throw "Invalid input"
+
+(display "(* 1 2 + 4 5) = ")
 (display (eval-exp '(* 1 2 + 4 5)))
